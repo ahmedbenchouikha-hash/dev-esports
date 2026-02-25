14a7f32401229cbb4be397a0692cd3e782ceec37
Jesser Hamdaoui <hamdaouijesser2004@gmail.com>
2026-02-26 00:59:53 +0100

    merge: resolve conflicts between module-tournament/acc1337e44f50a8d1f37ccf03b032d2743199d6e and current work

--- src/Controller/TournamentAdminController.php
+++ src/Controller/TournamentAdminController.php
@@ -6,25 +6,18 @@ use App\Entity\Tournament;
 use App\Form\TournamentType;
 use App\Repository\TournamentRepository;
 use Doctrine\ORM\EntityManagerInterface;
-<<<<<<< HEAD
 use Knp\Component\Pager\PaginatorInterface;
-=======
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-<<<<<<< HEAD
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Form\FormError;
-=======
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
 
 #[Route('/admin/tournaments', name: 'admin_tournament_')]
 class TournamentAdminController extends AbstractController
 {
     #[Route('', name: 'index', methods: ['GET'])]
-<<<<<<< HEAD
     public function index(TournamentRepository $tournamentRepository, Request $request, PaginatorInterface $paginator): Response
     {
         $search = trim((string) $request->query->get('search', ''));
@@ -35,22 +28,6 @@ class TournamentAdminController extends AbstractController
 
         $queryBuilder = $tournamentRepository->createAdminListQueryBuilder($search, $status, $sort, $direction);
         $tournaments = $paginator->paginate($queryBuilder, $page, 8);
-=======
-    public function index(TournamentRepository $tournamentRepository, Request $request): Response
-    {
-        $search = $request->query->get('search', '');
-        $status = $request->query->get('status', '');
-        $sort = $request->query->get('sort', 'startDate');
-        $direction = $request->query->get('direction', 'ASC');
-
-        if ($search) {
-            $tournaments = $tournamentRepository->findBySearchTerm($search, $sort, $direction);
-        } elseif ($status) {
-            $tournaments = $tournamentRepository->findByStatus($status, $sort, $direction);
-        } else {
-            $tournaments = $tournamentRepository->findAllOrdered($sort, $direction);
-        }
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
 
         return $this->render('admin/tournament/index.html.twig', [
             'tournaments' => $tournaments,
@@ -62,17 +39,12 @@ class TournamentAdminController extends AbstractController
     }
 
     #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
-<<<<<<< HEAD
     public function create(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
-=======
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
     {
         $tournament = new Tournament();
         $form = $this->createForm(TournamentType::class, $tournament);
         $form->handleRequest($request);
 
-<<<<<<< HEAD
         if ($form->isSubmitted()) {
             // Validate the tournament entity
             $errors = $validator->validate($tournament);
@@ -91,14 +63,6 @@ class TournamentAdminController extends AbstractController
                     $form->addError(new \Symfony\Component\Form\FormError($error->getMessage()));
                 }
             }
-=======
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($tournament);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Tournament created successfully!');
-            return $this->redirectToRoute('admin_tournament_show', ['id' => $tournament->getId()]);
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
         }
 
         return $this->render('admin/tournament/form.html.twig', [
@@ -109,16 +73,11 @@ class TournamentAdminController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
-<<<<<<< HEAD
     public function edit(Tournament $tournament, Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
-=======
-    public function edit(Tournament $tournament, Request $request, EntityManagerInterface $entityManager): Response
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
     {
         $form = $this->createForm(TournamentType::class, $tournament);
         $form->handleRequest($request);
 
-<<<<<<< HEAD
         if ($form->isSubmitted()) {
             // Validate the tournament entity
             $errors = $validator->validate($tournament);
@@ -137,14 +96,6 @@ class TournamentAdminController extends AbstractController
                     $form->addError(new \Symfony\Component\Form\FormError($error->getMessage()));
                 }
             }
-=======
-        if ($form->isSubmitted() && $form->isValid()) {
-            $tournament->setUpdatedAt(new \DateTime());
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Tournament updated successfully!');
-            return $this->redirectToRoute('admin_tournament_show', ['id' => $tournament->getId()]);
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
         }
 
         return $this->render('admin/tournament/form.html.twig', [
@@ -173,8 +124,4 @@ class TournamentAdminController extends AbstractController
 
         return $this->redirectToRoute('admin_tournament_index');
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1c04895fd40ddf3e3d0493c052d9fac6b47ed96e
